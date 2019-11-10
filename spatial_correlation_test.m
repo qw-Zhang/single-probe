17a9ef384b8e8c1ce3bdce3472f255c11fa310c3
qw-Zhang <411026539@qq.com>
2019-11-10 20:40:29 +0800

    add discrete six paths

--- spatial_correlation_test.m
+++ spatial_correlation_test.m
@@ -22,7 +22,8 @@ c = 3e8;
 lambda = c/fc;
 d = linspace(0,lambda,100);
 
-phi_a = 0*pi/180;scenario = 'macro';
+phi_a = 0*pi/180;
+scenario = 'macro';
 
 if isequal(scenario,'test')
     AOA = -22.5*pi/180;AS = 35*pi/180;
@@ -30,7 +31,7 @@ if isequal(scenario,'test')
 end
 if isequal(scenario,'micro')
     % % SCME urban micro-cell
-    AOA = [0.7,-13.2,146.1,-30.5,-11.4,-1.1]*pi/180;AS = 35*pi/180;
+    AOA = [0.7,-13.2,146.1,-30.5,-11.4,-1.1]*pi/180;AS = 35*pi/180;phi_a = 0*pi/180;
     pow = [-3,-4.3,-5.7,-7.3,-9,-11.4];
     amp = 10.^(pow/10);
     A = amp./max(amp);
@@ -38,7 +39,7 @@ if isequal(scenario,'micro')
         A(4)*exp(-abs(x-AOA(4))/AS)+A(5)*exp(-abs(x-AOA(5))/AS)+A(6)*exp(-abs(x-AOA(6))/AS));
 elseif isequal(scenario,'macro')
     % %SCME urban macro-cell
-    AOA = [65.7,45.6,143.2,32.5,-91.1,-19.2]*pi/180;AS = 35*pi/180;
+    AOA = [65.7,45.6,143.2,32.5,-91.1,-19.2]*pi/180;AS = 35*pi/180;phi_a = 0*pi/180;
     pow = [-3,-5.2,-4.7,-8.2,-12.1,-15.5];
     amp = 10.^(pow/10);
     A = amp./max(amp);
@@ -55,3 +56,30 @@ end
 
 figure;plot(d/lambda,abs(spatial));
 % grid on;
+
+%%
+%PAS continous->distrete    laplace->impulse
+AOA = round([0.7,-13.2,146.1,-30.5,-11.4,-1.1]);
+phi_a = 60*pi/180;
+pow = [-3,-4.3,-5.7,-7.3,-9,-11.4];
+amp = 10.^(pow/10);
+A = amp./max(amp);
+spatial = zeros(1,length(d));
+PAS = zeros(1,360);
+for i = 1:6
+    PAS(AOA(i)+180) = A(i);
+end
+% PAS = PAS*pi/180;
+sum_PAS = sum(PAS);
+phi_p = linspace(-pi,pi,360);
+
+for i = 1:100
+    temp = 0;
+    for j = 1:length(phi_p)
+        temp = temp + exp(-1i*2*pi*(d(i)/lambda)*sin(phi_p(j) - phi_a)).*PAS(j)/sum_PAS;
+    end
+    spatial(i) = temp;
+end
+
+figure;plot(d/lambda,abs(spatial));
+axis([0,1,0,1]);
